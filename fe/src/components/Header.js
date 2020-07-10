9ad52d460075aff22302aa28998464984da6ef3f
Taylor Cool <taycool@gmail.com>
2020-07-10 13:15:25 -0700

    adds navbar to fe

---
+++ fe/src/components/Header.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import theme from '../styles/theme';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+
+const Header = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color='primary' dark expand="md">
+        <NavbarBrand href="/">Fashion Blog</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/discover">Discover</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Account
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  My Profile
+                </DropdownItem>
+                <DropdownItem>
+                  My Settings
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Logout
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Header;
---
+++ fe/src/styles/theme.js
@@ -0,0 +1,11 @@
+const theme = {
+  colors: {
+    primary: '#264653',
+    secondary: '#2A9D8F',
+    danger: '#E76F51',
+    info: '#E9C46A',
+    warning: '#F4A261'
+  }
+}
+
+export default theme
